cf9b047e524d0e8f7b5530914db4f55fb4ed6b8e
Bruno Dias <dias.h.bruno@gmail.com>
2014-11-20 00:58:27 +0000

    fixed - sort posts by date.

--- lib/stc/PostComponent.php
+++ lib/stc/PostComponent.php
@@ -13,7 +13,8 @@ class PostComponent
       if ($post['type'] != 'post') continue;
       $fixed_posts[(int)format_date_YYYYMMDD($post['file'])] = $post;
     }
+
     sort($fixed_posts, SORT_NUMERIC);
-    Config::db()->store_data('post_list', $fixed_posts);
+    Config::db()->store('post_list', $fixed_posts);
   }
 }
